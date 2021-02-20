398cb1c2f3b01a8db85991be75b9b34c5b54dcaf
suenyeo <suenyeo@daeeunelec.com>
2021-02-20 20:23:39 +0900

    react study #

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CaredList } from './components/card-list/card-list.component.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();    
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users=> this.setState({monsters:users}));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input 
+          type='search' 
+          placeholder='search monsters' 
+          onChange={e => this.setState({ searchField : e.target.value})}
+        />
+        <CaredList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
 
 export default App;
