22128a571221f80fc79adbe41efb5ab441765723
suenyeo <suenyeo@daeeunelec.com>
2021-02-22 11:24:51 +0900

    add index styles

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   }
 
   handleChange = e => this.setState({ searchField : e.target.value})
-  
+
   render() {
     const {monsters, searchField} = this.state;
     const filteredMonsters = monsters.filter(monster =>
@@ -28,6 +28,7 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1> Monsters Rolodex </h1>
         <SearchBox 
           placeholder = 'search monsters'
           handleChange = {this.handleChange}
